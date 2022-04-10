e8a221d9855463087fd9a279e7d881053e302873
Janus Hasseriis <janus@hasseriis.net>
2022-04-10 13:54:43 +0200

    Removed boilerplate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // these are the variables you can use as inputs to your algorithms
-console.log(fxhash)   // the 64 chars hex number fed to your algorithm
-console.log(fxrand()) // deterministic PRNG function, use it instead of Math.random()
+console.log(fxhash); // the 64 chars hex number fed to your algorithm
+console.log(fxrand()); // deterministic PRNG function, use it instead of Math.random()
 
-// note about the fxrand() function 
+// note about the fxrand() function
 // when the "fxhash" is always the same, it will generate the same sequence of
 // pseudo random numbers, always
 
@@ -21,9 +21,3 @@ console.log(fxrand()) // deterministic PRNG function, use it instead of Math.ran
 // }
 
 // this code writes the values to the DOM as an example
-const container = document.createElement("div")
-container.innerText = `
-  random hash: ${fxhash}\n
-  some pseudo random values: [ ${fxrand()}, ${fxrand()}, ${fxrand()}, ${fxrand()}, ${fxrand()},... ]\n
-`
-document.body.prepend(container)
