9585b1341caf636d1e1f64fe444908b40b585dd7
Janus Hasseriis <janus@hasseriis.net>
2022-05-26 13:22:07 +0200

    Ready for release

--- public/sketch.js
+++ public/sketch.js
@@ -1,40 +1,101 @@
-//const w = window.innerWidth; //1000;
-//const h = window.innerHeight; //1000;
+let w, h;
+const aspect = 16 / 9;
+const grid = [];
+const listHexSizes = [1, 2, 4, 5, 10, 20];
+const noOfHex = listHexSizes[Math.floor(fxrand() * listHexSizes.length)];
 
-var w, h;
+const margin = false;
 
-var wh = (w = h = Math.min(window.innerWidth, window.innerHeight));
+let HexSize, HexSide, padding;
+
+let palettePick;
 
 function setup(params) {
-  c = createCanvas(wh, wh);
-  colorMode(HSB, 100);
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      let col = [i * 10, j * 10, 100];
-      let col2 = [i * 10 * fxrand(), j * 10 * fxrand(), 100];
-      let sqr1 = new Square(0.1 * w * i, 0.1 * h * j, col);
-      let sqr2 = new Square(0.05 * w + 0.1 * w * i, 0.1 * h * j, col2);
-      sqr1.show();
-      sqr2.show();
+  w = 1000;
+  h = w * aspect;
+
+  c = createCanvas(w, h);
+
+  HexSize = w / noOfHex;
+  HexSide = Math.ceil(HexSize / Math.sqrt(3));
+
+  let horizontalHexes = Math.floor(h / HexSize);
+  padding =
+    horizontalHexes === 1
+      ? -(1 / 9) * HexSize
+      : 0.5 * (h - horizontalHexes * HexSize);
+
+  if (fxrand() < 0.5) {
+    background(20);
+  } else {
+    background(240);
+  }
+  colorMode(HSB);
+  palettePick = Math.floor(colors.length * fxrand());
+  noStroke();
+  for (let j = 0; j < 1 + h / HexSize; j++) {
+    let row = [];
+    for (let index = 0; index < 1 + w / HexSize; index++) {
+      if (j % 2 === 0) {
+        row[index] = [index * HexSize, j * HexSize];
+      } else {
+        row[index] = [index * HexSize + 0.5 * HexSize, j * HexSize];
+      }
     }
+    grid[j] = row;
   }
 }
 
 function draw(params) {
+  for (let j = 0; j < grid.length; j++) {
+    for (let index = 0; index < grid[j].length; index++) {
+      let x = grid[j][index][0];
+      let y = padding + grid[j][index][1];
+      if (noOfHex != 1) {
+        if ((margin && j === 0) || (margin && j === grid.length - 1)) {
+          break;
+        }
+      }
+      push();
+      translate(x, y);
+      rotate(30 * (PI / 180));
+      drawHalfHex(0, 0, HexSide, pickColorScheme(), x);
+      rotate(PI);
+      drawHalfHex(0, 0, HexSide, pickColorScheme(), x);
+      rotate(PI);
+      drawHalfHex(0, 0, HexSide * 0.75, pickColorScheme(), x);
+      rotate(PI);
+      drawHalfHex(0, 0, HexSide * 0.75, pickColorScheme(), x);
+      rotate(PI);
+      drawHalfHex(0, 0, HexSide * 0.5, pickColorScheme(), x);
+      rotate(PI);
+      drawHalfHex(0, 0, HexSide * 0.5, pickColorScheme(), x);
+      pop();
+    }
+  }
   noLoop();
   fxpreview();
 }
 
-class Square {
-  constructor(x, y, col) {
-    this.x = x;
-    this.y = y;
-    this.col = col;
+function pickColorScheme() {
+  let colPick, color;
+  colPick = colors[palettePick];
+  color = colPick[Math.floor(colPick.length * fxrand())].hsb;
+  return color;
+}
 
-    this.show = function () {
-      fill(this.col);
-      noStroke();
-      rect(this.x, this.y, 0.05 * w, 0.1 * w);
-    };
+function drawHalfHex(x, y, len, col, realX) {
+  let gs = 0.5 * len;
+  if (col.length === 1) {
+    let hue = map(realX, 0, w, 0, 360);
+    fill(hue, 40 + len - 20 * fxrand(), 100 - 20 * fxrand());
+  } else {
+    fill(col);
   }
+  beginShape();
+  vertex(x - gs, y - sqrt(3) * gs);
+  vertex(x + gs, y - sqrt(3) * gs);
+  vertex(x + 2 * gs, y);
+  vertex(x + gs, y + sqrt(3) * gs);
+  endShape(CLOSE);
 }
