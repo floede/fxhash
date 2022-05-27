915b97563432d17115267be90c28f9cc0c59fc49
Janus Hasseriis <janus@hasseriis.net>
2022-05-27 09:07:00 +0200

    Border and webgl

--- public/sketch.js
+++ public/sketch.js
@@ -2,21 +2,25 @@ let w, h, bgColor;
 const aspect = 16 / 9;
 const grid = [];
 const listHexSizes = [1, 2, 4, 5, 10, 20];
-const noOfHex = listHexSizes[Math.floor(fxrand() * listHexSizes.length)];
+const noOfHex = 5; // listHexSizes[Math.floor(fxrand() * listHexSizes.length)];
 
 const margin = false;
+const border = 50;
 
 let HexSize, HexSide, padding;
 
 let palettePick;
 
 function setup(params) {
-  w = 1000;
-  h = w * aspect;
+  //w = 1000;
+  //h = w * aspect;
 
-  c = createCanvas(w, h);
+  h = min(aspect * windowWidth, windowHeight);
+  w = h / aspect;
 
-  HexSize = w / noOfHex;
+  c = createCanvas(w, h, WEBGL);
+
+  HexSize = (w - 2 * border) / noOfHex;
   HexSide = Math.ceil(HexSize / Math.sqrt(3));
 
   let horizontalHexes = Math.floor(h / HexSize);
@@ -35,13 +39,13 @@ function setup(params) {
   bgColor = colors[palettePick][0].hsb;
   background(bgColor);
   noStroke();
-  for (let j = 0; j < 1 + h / HexSize; j++) {
+  for (let j = 0; j < h / HexSize; j++) {
     let row = [];
-    for (let index = 0; index < 1 + w / HexSize; index++) {
+    for (let index = 0; index <= noOfHex; index++) {
       if (j % 2 === 0) {
-        row[index] = [index * HexSize, j * HexSize];
+        row[index] = [border + 0.5 * HexSize + index * HexSize, j * HexSize];
       } else {
-        row[index] = [index * HexSize + 0.5 * HexSize, j * HexSize];
+        row[index] = [border + index * HexSize, j * HexSize];
       }
     }
     grid[j] = row;
@@ -49,6 +53,7 @@ function setup(params) {
 }
 
 function draw(params) {
+  translate(-width / 2, -height / 2);
   for (let j = 0; j < grid.length; j++) {
     for (let index = 0; index < grid[j].length; index++) {
       let x = grid[j][index][0];
@@ -75,13 +80,20 @@ function draw(params) {
       pop();
     }
   }
-  strokeWeight(80);
+  /*   
+  strokeWeight(border);
   stroke(bgColor);
   noFill();
   rect(0, 0, width, height);
-  /*   strokeWeight(3);
+  strokeWeight(3);
   stroke(20);
   rect(20, 20, width - 40, height - 40); */
+  stroke(100);
+  strokeWeight(2);
+  line(border, 0, border, height);
+  line(width - border, 0, width - border, height);
+  line(0, border, width, border);
+  line(0, height - border, width, height - border);
   noLoop();
   fxpreview();
 }
